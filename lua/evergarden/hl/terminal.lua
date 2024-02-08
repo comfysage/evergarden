ffa58554c8806cdbfec3abe22882f5ad9e4a1290
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-02-08 16:28:29 +0100

    fix(hl/terminal): fix terminal highlights

--- lua/evergarden/hl/terminal.lua
+++ lua/evergarden/hl/terminal.lua
@@ -1,20 +1,20 @@
----@param theme OchaTheme
----@param _ OchaColors
-return function(theme, _)
-  vim.g.terminal_color_0  = theme.bg0[1]
-  vim.g.terminal_color_8  = theme.bg1[1]
-  vim.g.terminal_color_1  = theme.sakura[1]
-  vim.g.terminal_color_9  = theme.sakura[1]
-  vim.g.terminal_color_2 = theme.shinme[1]
-  vim.g.terminal_color_10  = theme.shinme[1]
-  vim.g.terminal_color_3 = theme.sakaeru[1]
-  vim.g.terminal_color_11  = theme.taiyo[1]
-  vim.g.terminal_color_4 = theme.sukai[1]
-  vim.g.terminal_color_12  = theme.sukai[1]
-  vim.g.terminal_color_5 = theme.seiun[1]
-  vim.g.terminal_color_13  = theme.seiun[1]
-  vim.g.terminal_color_6 = theme.ike[1]
-  vim.g.terminal_color_14  = theme.ike[1]
-  vim.g.terminal_color_7 = theme.bg3[1]
-  vim.g.terminal_color_15  = theme.fg2[1]
+---@param theme EvergardenTheme
+---@param colors EvergardenColors
+return function(theme, colors)
+  vim.g.terminal_color_0  = colors.bg0[1]
+  vim.g.terminal_color_8  = colors.bg1[1]
+  vim.g.terminal_color_1  = colors.red[1]
+  vim.g.terminal_color_9  = colors.red[1]
+  vim.g.terminal_color_2 = colors.green[1]
+  vim.g.terminal_color_10  = colors.green[1]
+  vim.g.terminal_color_3 = colors.yellow[1]
+  vim.g.terminal_color_11  = colors.orange[1]
+  vim.g.terminal_color_4 = colors.blue[1]
+  vim.g.terminal_color_12  = colors.blue[1]
+  vim.g.terminal_color_5 = colors.purple[1]
+  vim.g.terminal_color_13  = colors.purple[1]
+  vim.g.terminal_color_6 = colors.aqua[1]
+  vim.g.terminal_color_14  = colors.aqua[1]
+  vim.g.terminal_color_7 = colors.fg[1]
+  vim.g.terminal_color_15  = colors.grey1[1]
 end
